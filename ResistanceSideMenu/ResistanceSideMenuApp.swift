f37843f05728e592225bb9b56a3885affc533a92
Ivan Voznyi <ivan.voznyi92@gmail.com>
2025-09-27 10:01:12 +0300

    Initial Commit

---
+++ ResistanceSideMenu/ResistanceSideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResistanceSideMenuApp.swift
+//  ResistanceSideMenu
+//
+//  Created by Ivan Voznyi on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ResistanceSideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
